b15444af26fa69fde4b4eff73c685d6a4bb6b6bc
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-11-10 13:32:05 +0000

    base layout for home screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sustain',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      home: Products(),
+    );
+  }
+}
+
+class Products extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 100),
+          Container(
+            height: 250,
+            child: Container(color: Colors.red),
+          ),
+          SizedBox(height: 50),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Popular Products', style: TextStyle(fontSize: 20)),
+          ),
+          Container(
+            height: 100,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(width: 160, color: Colors.red),
+                Container(width: 160, color: Colors.blue),
+                Container(width: 160, color: Colors.green),
+                Container(width: 160, color: Colors.yellow),
+              ],
+            ),
+          ),
+          SizedBox(height: 50),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Recently Purchased', style: TextStyle(fontSize: 20)),
+          ),
+          Container(
+            height: 100,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(width: 160, color: Colors.red),
+                Container(width: 160, color: Colors.blue),
+                Container(width: 160, color: Colors.green),
+                Container(width: 160, color: Colors.yellow),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
