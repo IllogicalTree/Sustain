8fffdb44ae42667be1d4b370d31677c7510d8341
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-11-10 22:10:36 +0000

    adds register page

--- lib/register.dart
+++ lib/register.dart
@@ -4,17 +4,138 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Register Page'),
-          MaterialButton(
-            child: Center(
-              child: Text('Go to Login Page'),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFBEDB72), Color(0xFF3AE388)],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 30,
             ),
-            onPressed: () => Navigator.pushReplacementNamed(context, '/login'),
-          )
-        ],
+            Container(
+              height: 60,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/logo.png'),
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'REGISTER',
+              style: TextStyle(color: Colors.white, fontSize: 50),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+              ),
+              height: 40,
+              width: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    Icon(Icons.person, color: Colors.lightGreen),
+                    Text('Username'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+              ),
+              height: 40,
+              width: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    Icon(Icons.mail, color: Colors.lightGreen),
+                    Text('Email'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+              ),
+              height: 40,
+              width: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    Icon(Icons.lock, color: Colors.lightGreen),
+                    Text('Password'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 40,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+              ),
+              child: MaterialButton(
+                child: Center(
+                  child: Text('REGISTER!'),
+                ),
+                onPressed: () =>
+                    Navigator.pushReplacementNamed(context, '/products'),
+              ),
+            ),
+            Container(
+              child: FlatButton(
+                onPressed: () =>
+                    Navigator.pushReplacementNamed(context, '/login'),
+                child: Text(
+                  'Login Instead',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            Spacer(),
+            Container(
+              height: 294,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/register-image.png'),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
