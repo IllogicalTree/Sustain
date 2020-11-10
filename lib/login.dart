253211860ab2ac1cae664e5dc53c580dcdac9b74
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-11-10 19:30:47 +0000

    gradient testing

--- lib/login.dart
+++ lib/login.dart
@@ -4,18 +4,27 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Login Page'),
-          MaterialButton(
-            child: Center(
-              child: Text('Go to Products Page'),
-            ),
-            onPressed: () =>
-                Navigator.pushReplacementNamed(context, '/products'),
-          )
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFBEDB72), Color(0xFF3AE388)],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Login Page'),
+            MaterialButton(
+              child: Center(
+                child: Text('Go to Products Page'),
+              ),
+              onPressed: () =>
+                  Navigator.pushReplacementNamed(context, '/products'),
+            )
+          ],
+        ),
       ),
     );
   }
