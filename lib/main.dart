33b71326f207ec81334c963906c7f944d4ed3e13
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-11-13 17:31:35 +0000

    removes named product route

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:sustain/leaderboard.dart';
 import 'package:sustain/login.dart';
 import 'package:sustain/menu.dart';
 import 'package:sustain/news.dart';
-import 'package:sustain/product.dart';
 import 'package:sustain/profile.dart';
 import 'package:sustain/register.dart';
 import 'package:sustain/about.dart';
@@ -33,17 +32,6 @@ class MyApp extends StatelessWidget {
         '/profile': (context) => Profile(),
         '/menu': (context) => Menu(),
         '/history': (context) => History(),
-        /*
-        '/product': (context) => Product(
-              '5000112630633',
-              'coke can',
-              "Coca-Cola Classic is the world's favourite soft drink and has been enjoyed since 1886.",
-              'METAL - WIDELY RECYCLED',
-              0xFFDC596B,
-              'https://sustain-cos-standard-qpn.s3.eu-gb.cloud-object-storage.appdomain.cloud/Cola%20Can.png',
-              21,
-            ),
-            */
         '/about': (context) => About(),
         '/leaderboard': (context) => Leaderboard(),
         '/news': (context) => News(),
